e8509393bd25320c2cef288bb89d7d1d16c480fc
Max David <me@maxdavid.com>
2013-07-01 00:08:31 -0400

    Fixing some percentage parsing. 'Charging' is just listed as 'Unknown' on the lappy, probably a firmware lapse.

--- .bin/battery.sh
+++ .bin/battery.sh
@@ -7,19 +7,19 @@ red=#FF8980
 lowthreshold=15
 
 # This line sets batpercent as the remaining battery life as a percentage
-batpercent=$(acpi -b | awk '{print substr($4,1,length($4)-1)}')
+batpercent=$(acpi -b | awk '{print $4}' | sed 's/,$//g')
 # Set batnumber as the percentage number, without the % sign
-batnumber=$(echo $batpercent | awk 'sub(".$", "")')
+batnumber=$(echo $batpercent | sed 's/%$//g')
 
 # This line sets batstate as the battery state (Charging/Discharging/AC)
-batstate=$(acpi -b | awk '{print substr($3,1,length($3)-1)}')
+batstate=$(acpi -b | awk '{print $3}' | sed 's/,$//g')
 
 # if: the battery is charging AND it's not 100%, then: set percentage to green color
 # elif: the battery is less than 15%, then: set percentage to red color
 # else: just print the damn percentage
-if [ "$batstate" = "Charging" || "AC" ] && [ "$batpercent" != "100" ]; then
+if [ "$batstate" = "Unknown" ] && [ "$batnumber" != "100" ]; then
 	echo "<fc=$green>"$batpercent"</fc>"
-elif [ "$batnumber" -le "$lowthreshold" ] && [ "$batpercent" != "100" ]; then
+elif [ "$batnumber" -le "$lowthreshold" ] && [ "$batnumber" != "100" ]; then
 	echo "<fc=$red>"$batpercent"</fc>"
 else
 	echo $batpercent
