97d7db759955ee9432b47c3f87328141d625f013
Max David <me@maxdavid.com>
2018-02-16 19:38:44 -0800

    Stow fix, must manually run command

--- install.sh
+++
@@ -1,3 +0,0 @@
-for d in $(ls); do stow $d; done
-# To install these dotfiles without Git:
-# cd $HOMEDIR; curl -#L https://github.com/maxdavid/dotfiles/tarball/master | tar -xzv --strip-components 1
